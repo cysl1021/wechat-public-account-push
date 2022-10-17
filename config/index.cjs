2c9bf1c3200c2752e2451306744e058938250b5c
cysl1021 <115975627+cysl1021@users.noreply.github.com>
2022-10-17 16:17:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oj-qK6K3WgX6ZDvBzcb3X5bibC2w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	zarWWhPHOo54e6sUfFX0KOUDh9ohaOce2xQ0nN8desA',
+      useTemplateId: '	q4pLOqgVJnNRxLnRoJv5NqRxPwYPrX1NEM3_br7GqJg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
